48cc63702a8fc77f123b47f708af6de2e499a626
Betsiha Yayeh <yayehares@gmail.com>
2025-11-28 23:53:24 +0300

    uppdated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-// POST /api/login
+
 router.post("/login", authController.login);
 
 module.exports = router;
