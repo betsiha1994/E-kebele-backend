a553f72ff240a31225627652d55a1e490e77dd29
Betsiha Yayeh <yayehasres@gmail.com>
2025-11-29 09:39:18 +0300

    updated

--- services/serviceService.js
+++ services/serviceService.js
@@ -2,16 +2,15 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 
+const createService = async (data) => {
+  return await prisma.service.create({ data });
+};
 const getAllServices = async () => {
   return await prisma.service.findMany();
 };
 
 const getServiceById = async (id) => {
   return await prisma.service.findUnique({ where: { id: parseInt(id) } });
-};
-
-const createService = async (data) => {
-  return await prisma.service.create({ data });
 };
 
 const updateService = async (id, data) => {
