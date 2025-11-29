a2307a35714cbe3a0a72310b085a431d8b115f62
Betsiha Yayeh <yayehasres@gmail.com>
2025-11-29 20:07:35 +0300

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,13 +12,12 @@ async function login(req, res) {
     if (!user) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
-
-    // 2. Verify password
+    
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
-    
+
     const token = jwt.sign(
       { id: user.id, email: user.email, role: user.role },
       process.env.JWT_SECRET,
