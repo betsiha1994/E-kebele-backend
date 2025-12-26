6df4fd4938c2b85f6fbe9117e1cb68f9afde30a8
Betsiha Yayeh <yayehasres@gmail.com>
2025-12-26 19:52:11 +0300

    uppdated

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
-const authMiddleware = require("../middleware/authMiddleware"); // JWT auth
+const authMiddleware = require("../middleware/authMiddleware"); 
 // const adminOnly = require("../middlewares/adminOnly"); // optional: admin-only
 
 
